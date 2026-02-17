3c8e3236abd31440a360c35c9e9f14f35b7e93b3
hexter2018 <phongthep.kd@gmail.com>
2026-02-17 10:45:56 +0700

    Fix missing bg-success-600 class

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:{
+          50: '#eff6ff',
+          300: '#93c5fd',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#1e40af',
+        },
+        success: {
+          600: '#16a34a',
+          700: '#15803d',
+        },
+        danger: {
+          600: '#dc2626',
+          700: '#b91c1c',
+        },
+      },
+    },
   },
   plugins: [],
 };
